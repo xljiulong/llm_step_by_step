c36a9c93cee9568ca5047bd62938a71010ae19ad
zhangjl19 <zhangjl19@spdb.com.cn>
2024-05-09 10:31:52 +0800

    fix gqa

--- src/model/llama2/modeling_llama2.py
+++ src/model/llama2/modeling_llama2.py
@@ -253,6 +253,7 @@ class LlamaAttention(nn.Module):
         self.config = config
         self.hidden_size = config.hidden_size
         self.num_heads = config.num_attention_heads
+        self.num_heads = config.num_heads
         self.head_dim = self.hidden_size // self.num_heads
         self.num_key_value_heads = config.num_key_value_heads
         self.num_key_value_groups = self.num_heads // self.num_key_value_heads
